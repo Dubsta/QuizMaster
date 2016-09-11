54fbbbaf4f2c6bb182f179c281e2b4560b7ffc43
Greg Duncan <gregdd@outlook.com>
2016-09-11 22:09:23 +1000

    add last 2 of 10 questions

--- questions.js
+++ questions.js
@@ -15,7 +15,7 @@ var rawQuiz0 =
     ["What colour pill did Neo choose in the Matrix?", ["Red", "Blue", "Green", "Yellow"] ],
     ["What year did WW1 start?", ["1914", "1913", "1912", "1915"] ],
     ["What two colours do you mix to get Green?", ["Blue & Yellow", "Red & Blue", "Red & Yellow", "Red & Purple"] ],
-    ["Which of these countries is not in Central Amercia?", ["Mexico", "Panama", "Costa Rica", "Belize"] ]
-
-    
+    ["Which of these countries is not in Central Amercia?", ["Mexico", "Panama", "Costa Rica", "Belize"] ],
+    ["Which film beat Star Wars (the orginal) for Best Picture?", ["Annie Hall", "Rocky", "Close Encounters", "Kramer v Kramer"]],
+    ["Who is the patron saint for travellers?", ["St. Christopher", "St. Patrick", "St. Valentine", "St. Clare"]]   
 ];
